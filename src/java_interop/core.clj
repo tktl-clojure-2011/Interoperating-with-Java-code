3c9206a0f78516be7dca63735c1990e82ac7d84e
Juhana Laurinharju <fingerzam@gmail.com>
2011-02-17 19:46:05 +0200

    Facts for randoms and arraylist-flatten

---
+++ src/java_interop/core.clj
@@ -0,0 +1,9 @@
+(ns java-interop.core)
+
+(defn randoms [seed a-count]
+  ":(")
+
+(defn arraylist-flatten [seq-of-seqs]
+  ":(")
+
+;^_^
---
+++ test/java_interop/test_core.clj
@@ -0,0 +1,17 @@
+(ns java-interop.test-core
+  (:use java-interop.core
+        midje.sweet))
+
+(facts "randoms"
+       (randoms 1 3)
+         => [-1155869325 431529176 1761283695]
+       (randoms 1337 5)
+         => [-1460590454 747279288 -1334692577 -539670452 -501340078])
+
+(facts "arraylist-flatten"
+       (seq (arraylist-flatten [])) => []
+       (seq (arraylist-flatten [[]])) => []
+       (seq (arraylist-flatten [[1 2]])) => [1 2]
+       (seq (arraylist-flatten [[1 2 3] [4 5 6]])) => [1 2 3 4 5 6]
+       (.getClass (arraylist-flatten [[1 2 3] [4 5 6]])) 
+           => java.util.ArrayList)
