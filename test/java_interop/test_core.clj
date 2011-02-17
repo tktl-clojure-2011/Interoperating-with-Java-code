9b0c11bb4612bd62bd05be1d83a3bb57e1672fd7
Juhana Laurinharju <fingerzam@gmail.com>
2011-02-17 19:53:23 +0200

    Fix facts for arraylist-flatten

--- test/java_interop/test_core.clj
+++ test/java_interop/test_core.clj
@@ -9,8 +9,8 @@
          => [-1460590454 747279288 -1334692577 -539670452 -501340078])
 
 (facts "arraylist-flatten"
-       (seq (arraylist-flatten [])) => []
-       (seq (arraylist-flatten [[]])) => []
+       (seq (arraylist-flatten [])) => empty?
+       (seq (arraylist-flatten [[]])) => empty?
        (seq (arraylist-flatten [[1 2]])) => [1 2]
        (seq (arraylist-flatten [[1 2 3] [4 5 6]])) => [1 2 3 4 5 6]
        (.getClass (arraylist-flatten [[1 2 3] [4 5 6]])) 
