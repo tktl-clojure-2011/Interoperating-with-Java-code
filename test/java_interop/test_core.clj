b4c11498fce9d6ba942fae5e7e49086f0da85a25
Juhana Laurinharju <fingerzam@gmail.com>
2011-02-17 20:32:06 +0200

    New fact for comparing

--- test/java_interop/test_core.clj
+++ test/java_interop/test_core.clj
@@ -26,6 +26,7 @@
        (minimum-str ["OMG" "ponies" ""]) => "")
 
 (facts "comparing"
+       (Collections/min #{"abc" "cat" ":D"} (comparing count)) => ":D"
        (.compare (comparing #(get {:a 1 :b 2} %)) :b :a) => 1
        (.compare (comparing #(get {:a 1 :b 2} %)) :a :b) => -1
        (instance? Comparator (comparing (partial * 2))) => true
